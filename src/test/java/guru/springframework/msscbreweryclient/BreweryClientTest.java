8be5845631577b83d4f915051761f0f60ba253fc
awash <Emerald190!!>
2024-08-29 19:33:22 -0500

    add restTemplate get

---
+++ src/test/java/guru/springframework/msscbreweryclient/BreweryClientTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscbreweryclient;
+
+import guru.springframework.msscbreweryclient.web.client.BreweryClient;
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+
+    }
+}
